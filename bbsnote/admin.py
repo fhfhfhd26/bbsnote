e2c40548cd16c12112234d3ec785486a40ea1a43
SSONG <fhfhfhd26@gmail.com>
2021-03-21 15:52:59 +0900

    bbsnote first commit

---
+++ bbsnote/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Board, Comment
+
+# Register your models here.
+class BoardAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+class CommentAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+admin.site.register(Board, BoardAdmin)
+admin.site.register(Comment, CommentAdmin)
